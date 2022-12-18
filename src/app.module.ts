65909ac8109ac4b36d48c8a149d6362e3000b896
Marlon <Marlonnlmx@outlook.com>
2022-12-18 18:52:34 -0300

    Código da quarta aula

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from './infra/http/http.module';
 import { DataBaseModule } from './infra/database/database.module';
+import { MessagingModule } from '@infra/messaging/messaging.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule, DataBaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    HttpModule,
+    DataBaseModule,
+    MessagingModule,
+  ],
 })
 export class AppModule {}
